330b9263c4901c420ab4e43bbb95c4f4e0fd7e20
Elinpf <365433079@qq.com>
2018-05-24 10:25:50 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# 返回一个完整的title
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			return base_title
+		else
+			return page_title + " | " + base_title
+		end
+	end
 end
